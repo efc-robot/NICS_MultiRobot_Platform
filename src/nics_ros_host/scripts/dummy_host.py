b1850395a41d95d40f4d644054398bc27a82b9df
Jiantao Qiu <qjt15@mails.tsinghua.edu.cn>
2021-06-11 01:32:20 +0800

    try new host style

--- src/nics_ros_host/scripts/dummy_host.py
+++ src/nics_ros_host/scripts/dummy_host.py
@@ -1,20 +1,49 @@
 #!/usr/bin/python3
 
+import threading
 import time
+from rosgraph import xmlrpc
 import rospy
 import numpy as np
 import argparse 
 import rosnode
+import copy
+import threading
 
-from fake_host.srv import *
+from nics_ros_host.srv import *
+from MultiVehicleEnv.environment import MultiVehicleEnv
+import MultiVehicleEnv.scenarios as scenarios
+
+def make_env(scenario_name, args):
+    # load scenario from script
+    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    # create world
+    world = scenario.make_world(args)
+    # create multiagent environment
+
+    env = MultiVehicleEnv(world, scenario.reset_world, scenario.reward, scenario.observation,scenario.info)
+    return env
+
+class pos_data(object):
+    def __init__(self,x,y,theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
 
 class FakeHost(object):
-    def __init__(self, args):
-        self.agent_num = args.agent_num
-        rospy.init_node("Fake_ros_host")
+    def __init__(self, args, env:MultiVehicleEnv):
+        # get agent number from env
+        self.agent_num = len(env.vehicle_list)
+        # init host node
+        rospy.init_node("robot_host")
+
+        # TODO get this param from launch file
+        self.core_fps = 10
+
+        # check the number of agent client 
         All_ready = False
         while not All_ready:
-            node_name_list:str = rosnode.get_node_names()
+            node_name_list:list[str] = rosnode.get_node_names()
             self.car_id_list = []
             for node_name in node_name_list:
                 if node_name.endswith('robot_client'):
@@ -25,29 +54,73 @@ class FakeHost(object):
                 break
             print(self.car_id_list)
             time.sleep(0.5)
-        
+        #build observation services
         self.obs_server_list = []
         for car_id in self.car_id_list:
             handle = lambda req: self.obs_calculate(car_id,req)
-            obs_messenger = rospy.Service(car_id+'/get_obs', obs, handle)
+            obs_messenger = rospy.Service('/'+car_id+'/get_obs', obs, handle)
         
+        self.env = env
+        self.vrpn_list = [pos_data(0,0,0) for _ in range(self.agent_num)]
+
+
+
+        self.core_thread = threading.Thread(target=self.core_function)
         rospy.spin()
 
     def obs_calculate(self,car_id,req):
         rospy.loginfo("Calculate obs for car %s",car_id)
-        
-        dummy_obs = np.random()
-        return obsResponse([2],[2],[2])
+        car_index = self.car_id_list.index(car_id)
+        agent = self.env.vehicle_list[car_index]
+        obs_result = self.env._get_obs(agent)     
+        return obsResponse(obs_result)
+
+    def core_function(self):
+        while True:
+            old_movable_list = copy.deepcopy([v.state.movable for v in self.env.vehicle_list])
+            self._update_world()
+            for v_idx in range(self.agent_num):
+                v = self.env.vehicle_list[v_idx]
+                if not(v.state.movable == old_movable_list[v_idx]):
+                    #TODO call v_idx vehicle set state service
+                    pass
+            time.sleep(self.core_fps)
+
+    def _update_world(self):
+        self._set_state_callback()
+        #　TODO set correct total time
+        total_time = 0.0
+        self.env.ros_step(total_time)
+
+    def _set_state_callback(self):
+        for vehicle_idx in range(self.agent_num):
+            state = self.env.vehicle_list[vehicle_idx].state
+            vrpn_data = self.vrpn_list[vehicle_idx]
+            state.coordinate[0] = vrpn_data.x
+            state.coordinate[1] = vrpn_data.y
+            state.theta = vrpn_data.theta
+
+
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="Fake vrpn")
-    parser.add_argument('--agent-num',type=int,default=1)
+
+    parser = argparse.ArgumentParser(description="GUI for Multi-VehicleEnv")
+    parser.add_argument('--gui-port',type=str,default='/dev/shm/gui_port')
+    parser.add_argument('--usegui', action='store_true', default=False)
+    parser.add_argument('--step-t',type=float,default=1.0)
+    parser.add_argument('--sim-step',type=int,default=100)
+    parser.add_argument('--direction_alpha', type=float, default=1.0)
+    parser.add_argument('--add_direction_encoder',type=str, default='train')
+
+ 
     try:
         args = parser.parse_args()
     except:
         args = parser.parse_args([])
     else:
         args = parser.parse_args()
-    
-    fake_host = FakeHost(args)
+
+
+    env = make_env('3p2t2f', args)
+    fake_host = FakeHost(args, env)
