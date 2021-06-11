0cb482b884c15063c22c8f28383a0cadf03315d8
Jiantao Qiu <qjt15@mails.tsinghua.edu.cn>
2021-06-11 17:52:53 +0800

    ns

--- src/mac2id.py
+++ src/mac2id.py
@@ -1,19 +1,21 @@
 import uuid
-import os
 def get_mac_address(): 
     mac=uuid.UUID(int = uuid.getnode()).hex[-12:] 
     return ":".join([mac[e:e+2] for e in range(0,11,2)])
 
 def mac2id(mac):
-    mac2id_table = {'e4:5f:01:00:d4:20':'1',
-                    'e4:5f:01:00:d3:d8':'2',
-                    'e4:5f:01:00:d4:5c':'3',
-                    'e4:5f:01:00:d3:ae':'4'}
+    mac2id_table = {}
+    mac2id_table['e4:5f:01:00:d4:20'] = 'car_1'
+    mac2id_table['e4:5f:01:00:d3:d8'] = 'car_2'
+    mac2id_table['e4:5f:01:00:d4:5c'] = 'car_3'
+    mac2id_table['e4:5f:01:00:d3:ae'] = 'car_4'
+    mac2id_table['e0:d5:5e:26:58:85'] = 'host_0'
+    
     if mac in mac2id_table.keys():
         return mac2id_table[mac]
     else:
         return 'dummy'
 
 if __name__ == '__main__':
-    print('car_'+mac2id(get_mac_address()))
+    print(mac2id(get_mac_address()))
     
