bf96e984c8b341c25cc54d6a8f4a4d2770a8dd71
Jiantao Qiu <qjt15@mails.tsinghua.edu.cn>
2021-06-11 18:26:47 +0800

    update client setup

--- scripts/client_setup.sh
+++ scripts/client_setup.sh
@@ -1,5 +1,6 @@
-catkin_make  -DCATKIN_WHITELIST_PACKAGES=xtark_driver
-catkin_make  -DCATKIN_WHITELIST_PACKAGES=robot_client
+export ROS_MASTER_URI=http://172.16.1.11:11311
+catkin_make --only-pkg-with-deps robot_client xtark_driver
 source devel/setup.sh
 export CAR_ID=`python src/mac2id.py`
-roslaunch xtark_driver xtark_driver.launch
+roslaunch xtark_driver xtark_driver.launch &
+roslaunch robot_client robot_client.launch &
